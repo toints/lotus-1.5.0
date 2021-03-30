dec11e2f94916fe45c33cbbe92e748b55cd6adfa
moran666666 <66150364+moran666666@users.noreply.github.com>
2021-03-30 09:48:30 +0800

    Update sealer_cgo.go
    add extern c2

--- extern/sector-storage/ffiwrapper/sealer_cgo.go
+++ extern/sector-storage/ffiwrapper/sealer_cgo.go
@@ -5,9 +5,14 @@ package ffiwrapper
 import (
 	"bufio"
 	"bytes"
+	"compress/gzip"
 	"context"
+	"encoding/json"
+	"fmt"
 	"io"
+	"io/ioutil"
 	"math/bits"
+	"net/http"
 	"os"
 	"runtime"
 
@@ -564,6 +569,10 @@ func (sb *Sealer) SealCommit1(ctx context.Context, sector storage.SectorRef, tic
 }
 
 func (sb *Sealer) SealCommit2(ctx context.Context, sector storage.SectorRef, phase1Out storage.Commit1Out) (storage.Proof, error) {
+	if c2Address, ok := os.LookupEnv("C2_ADDRESS"); ok {
+		log.Warnf("Sector %d do the extern commit2 task ......", sector.ID.Number)
+		return RequestCommit2(sector.ID, phase1Out, c2Address)
+	}
 	return ffi.SealCommitPhase2(phase1Out, sector.ID.Number, sector.ID.Miner)
 }
 
@@ -716,4 +725,76 @@ func GenerateUnsealedCID(proofType abi.RegisteredSealProof, pieces []abi.PieceIn
 	}
 
 	return ffi.GenerateUnsealedCID(proofType, allPieces)
+}
+
+type Commit2Request struct {
+	SectorID   abi.SectorID
+	Commit1Out storage.Commit1Out
+}
+type Commit2Response struct {
+	SectorID abi.SectorID
+	Proof    storage.Proof
+}
+
+func RequestCommit2(sector abi.SectorID, phase1Out storage.Commit1Out, c2Address string) (storage.Proof, error) {
+	request := Commit2Request{}
+	request.SectorID = abi.SectorID{
+		Miner:  abi.ActorID(sector.Miner),
+		Number: abi.SectorNumber(sector.Number),
+	}
+	request.Commit1Out = phase1Out
+
+	b, err := json.Marshal(request)
+	if err != nil {
+		log.Errorf("json format error: %+v", err)
+		return nil, err
+	}
+
+	zBuf := new(bytes.Buffer)
+	zw := gzip.NewWriter(zBuf)
+	if _, err = zw.Write(b); err != nil {
+		log.Errorf("commit2 gzip failed, error: %+v", err)
+	}
+
+	zw.Flush()
+	zw.Close()
+
+	req, err := http.NewRequest("POST", fmt.Sprintf("http://%s/proxy/commit2", c2Address), zBuf)
+	if err != nil {
+		log.Errorf("http new request err: %+v", err)
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/json;charset=utf-8")
+	req.Header.Set("Content-Encoding", "gzip")
+
+	log.Info("Send commit2 task to extern server and wait for response ......")
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Error("do request err: ", err)
+		return nil, err
+	}
+
+	if resp.StatusCode != 200 {
+		log.Errorf("http client do code: %d err: %+v", resp.StatusCode, err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	result, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Errorf("ioutil read all err: %+v", err)
+		return nil, err
+	}
+
+	response := Commit2Response{}
+	err = json.Unmarshal(result, &response)
+	if err != nil {
+		log.Errorf("json format len: %d error: %+v", len(result), err)
+		return nil, err
+	}
+
+	log.Info("Receive commit2 result success......")
+
+	return response.Proof, nil
 }
